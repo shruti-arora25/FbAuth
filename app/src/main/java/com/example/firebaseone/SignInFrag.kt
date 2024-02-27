1fefabadc2bed408ef77694dc687c0c7658bcbb5
Shruti <shrutiarora2507@gmail.com>
2024-02-27 19:11:41 +0530

    Comit

--- app/src/main/java/com/example/firebaseone/SignInFrag.kt
+++ app/src/main/java/com/example/firebaseone/SignInFrag.kt
@@ -1,11 +1,16 @@
 package com.example.firebaseone
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.example.firebaseone.databinding.FragmentSignInBinding
@@ -55,6 +60,42 @@ class SignInFrag : Fragment() {
 
         bind.createAccount.setOnClickListener {
             findNavController().navigate(R.id.action_signInFrag_to_signUpFrag,null,NavOptions.Builder().setPopUpTo(R.id.signUpFrag,true).build())
+        }
+
+        bind.ForgotPass.setOnClickListener {
+            val builder= context?.let { it1 -> AlertDialog.Builder(it1) }
+            val view=layoutInflater.inflate(R.layout.dialogbox,null)
+            val userEmail=view.findViewById<EditText>(R.id.editTextTextEmailAddress)
+
+
+            builder?.setView(view)
+            val dialog=builder?.create()
+            view.findViewById<Button>(R.id.reset).setOnClickListener {
+                compareEMail(userEmail)
+                dialog?.dismiss()
+            }
+            view.findViewById<Button>(R.id.cancel).setOnClickListener {
+                dialog?.dismiss()
+            }
+
+            if (dialog?.window!=null){
+                dialog.window!!.setBackgroundDrawable(ColorDrawable(1))
+            }
+            dialog?.show()
+        }
+
+    }
+
+    private fun compareEMail(email:EditText){
+        if (email.text.toString().isEmpty()){
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()){
+            return
+        }
+        fbAuth.sendPasswordResetEmail(email.text.toString()).addOnCompleteListener {
+            if (it.isSuccessful)
+                Toast.makeText(context,"Check Your Mail",Toast.LENGTH_SHORT).show()
         }
 
     }
